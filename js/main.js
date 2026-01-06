fce40da4a15ab2911880eabfef43af8018b312d3
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-06 16:30:11 +0100

    added "peso medio"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log("hello");
+let pesiBase = [2, 5, 1, 1, 7, 3, 12, 6, 9, 3];
+console.table(pesiBase);
+
+// chiedo all'utente quale valore vuole conoscere
+let task = prompt(`chiedi tra: "media", "max", "min", "num"`);
+
+// peso medio
+if (task === "media") {
+  let sommaPesi = 0;
+
+  for (let i = 0; i < pesiBase.length; i++) {
+    let pesoCorrente = pesiBase[i];
+    sommaPesi += pesoCorrente;
+  }
+
+  let mediaPesi = sommaPesi / pesiBase.length;
+  console.log("Peso medio: " + mediaPesi);
+  alert("Peso medio: " + mediaPesi);
+}
+
+// peso massimo
+
+// peso minimo
+
+// numero di giocattoli
